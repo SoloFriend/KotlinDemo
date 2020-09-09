dbc79cb8802a0b5cb4562ac1fb25984cebc287f7
shengyt <shengyuting@hc360.com>
2020-09-09 10:05:59 +0800

    @20200909 syt【gallery】修改 GalleryViewModel.kt -> fetchData: 所有判断条件都通过后，再将isLoading修改为true

--- gallery/src/main/java/com/syt/gallery/vm/GalleryViewModel.kt
+++ gallery/src/main/java/com/syt/gallery/vm/GalleryViewModel.kt
@@ -67,11 +67,11 @@ class GalleryViewModel(application: Application) : AndroidViewModel(application)
 
     fun fetchData() {
         if (isLoading) return
-        isLoading = true
         if (currentPage > totalPage) {
             _dataStatusLive.value = DATA_STATUS_NO_MORE
             return
         }
+        isLoading = true
         val stringRequest = StringRequest(
             Request.Method.GET,
             getUrl(),
