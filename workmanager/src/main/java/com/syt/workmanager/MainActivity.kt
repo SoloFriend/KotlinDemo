46553c5b88083630a5ead5fe83204f9502ddc26f
shengyt <shengyuting@hc360.com>
2020-09-25 19:13:19 +0800

    @20200925 syt【work manager】添加work manager 控制逻辑

--- workmanager/src/main/java/com/syt/workmanager/MainActivity.kt
+++ workmanager/src/main/java/com/syt/workmanager/MainActivity.kt
@@ -2,16 +2,22 @@ package com.syt.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val workManager = WorkManager.getInstance(this)
-        val workRequest = OneTimeWorkRequestBuilder<TestWorker>().build()
-        workManager.enqueue(workRequest)
+        val workManager = WorkManager.getInstance(applicationContext)
+        val workRequest1 = OneTimeWorkRequestBuilder<TestWorker>().build()
+        button.setOnClickListener {
+            workManager.enqueue(workRequest1)
+        }
+
+        workManager.getWorkInfoByIdLiveData(workRequest1.id).observe(this) {
+            textView.text = it.state.toString()
+        }
     }
 }
