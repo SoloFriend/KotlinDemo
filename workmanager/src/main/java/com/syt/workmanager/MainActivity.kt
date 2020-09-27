be92454aca095f5fb93bcc2021d3de2b94c4a40e
shengyt <shengyuting@hc360.com>
2020-09-27 18:58:53 +0800

    @20200927 syt【Worker】测试Worker级联任务

--- workmanager/src/main/java/com/syt/workmanager/MainActivity.kt
+++ workmanager/src/main/java/com/syt/workmanager/MainActivity.kt
@@ -2,8 +2,14 @@ package com.syt.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.work.*
 import kotlinx.android.synthetic.main.activity_main.*
+
+const val KEY_INPUT = "input_key"
+const val KEY_OUTPUT = "output_key"
+const val WORK_NAME_ONE = "work_one"
+const val WORK_NAME_TWO = "work_two"
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,13 +17,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val workManager = WorkManager.getInstance(applicationContext)
-        val workRequest1 = OneTimeWorkRequestBuilder<TestWorker>().build()
+        val workRequest1 = createWorkRequest(WORK_NAME_ONE)
+        val workRequest2 = createWorkRequest(WORK_NAME_TWO)
         button.setOnClickListener {
-            workManager.enqueue(workRequest1)
+            workManager.beginWith(workRequest1).then(workRequest2).enqueue()
         }
 
         workManager.getWorkInfoByIdLiveData(workRequest1.id).observe(this) {
-            textView.text = it.state.toString()
+            Log.w("WorkActivity", "onCreate: " + it.state.toString())
+            if (it.state == WorkInfo.State.SUCCEEDED) {
+                textView.text = it.outputData.getString(KEY_OUTPUT).plus(it.state.toString())
+            }
         }
+        workManager.getWorkInfoByIdLiveData(workRequest2.id).observe(this) {
+            Log.w("WorkActivity", "onCreate: " + it.state.toString())
+            if (it.state == WorkInfo.State.SUCCEEDED) {
+                textView.text = it.outputData.getString(KEY_OUTPUT).plus(it.state.toString())
+            }
+        }
+    }
+
+    private fun createWorkRequest(name: String): OneTimeWorkRequest {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+        return OneTimeWorkRequestBuilder<TestWorker>()
+            .setConstraints(constraints)
+            .setInputData(workDataOf(KEY_INPUT to name))
+            .build()
     }
 }
--- workmanager/src/main/java/com/syt/workmanager/TestWorker.kt
+++ workmanager/src/main/java/com/syt/workmanager/TestWorker.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 
 class TestWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
-    private val TAG ="TestWorker"
+    private val TAG = "TestWorker"
     override fun doWork(): Result {
-        Log.w(TAG, "doWork: started" )
+        val name = inputData.getString(KEY_INPUT)
+        Log.w(TAG, "doWork: $name started")
         Thread.sleep(3000)
-        Log.w(TAG, "doWork: finished" )
-        return Result.success()
+        Log.w(TAG, "doWork: $name finished")
+        return Result.success(workDataOf(KEY_OUTPUT to name))
     }
 }
